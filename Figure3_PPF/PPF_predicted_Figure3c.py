ef769591841498d1231f64390a6bf5f68c204faf
furan2019 <51429163+furan2019@users.noreply.github.com>
2025-02-18 21:41:23 +0800

    Add files via upload

---
+++ Figure3_PPF/PPF_predicted_Figure3c.py
@@ -0,0 +1,166 @@
+import argparse, pickle, os, joblib
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle, resample
+from baggingPU import BaggingClassifierPU
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, recall_score, f1_score, mean_squared_error, confusion_matrix
+
+def GetProdictPS_lgb(x_train, y_train, x_test, y_test):
+    classifier = LGBMClassifier(objective='binary', boosting_type='dart')
+    rf = classifier
+    rf.fit(x_train, y_train)
+    y_pred = rf.predict(x_test)    
+    accuracy = accuracy_score(y_pred, y_test)
+    recall = recall_score(y_pred, y_test)
+    f1_s = f1_score(y_pred, y_test)
+    return accuracy, recall, f1_s
+
+def GetProdictPS_pu(X_test, X_train, Y_test, Y_train):
+    estimator = LGBMClassifier(objective='binary', boosting_type='dart')
+    bc = BaggingClassifierPU(base_estimator=estimator)
+    bc.fit(X_train, Y_train)
+    Y_pred = bc.predict(X_test)
+    accuracy = accuracy_score(Y_pred, Y_test)
+    recall = recall_score(Y_pred, Y_test)
+    f1_s = f1_score(Y_pred, Y_test)
+    return accuracy, recall, f1_s
+
+Ps_df_org = pd.read_csv("PS_7features+PPF_predicted.csv", index_col=0)
+Nops_df_org = pd.read_csv("NonPS_7features+PPF_predicted.csv", index_col=0)
+
+mean_values_ps = Ps_df_org.mean()
+Pas_all_filled = Ps_df_org.fillna(mean_values_ps)
+mean_values_no = Nops_df_org.mean()
+Nops_all_filled = Nops_df_org.fillna(mean_values_no)
+
+accuracy_f8_lgb_re10, accuracy_f8_pu_re10, accuracy_f1_lgb_re10,  accuracy_f7_lgb_re10= (
+    pd.DataFrame(data=None), pd.DataFrame(data=None), pd.DataFrame(data=None), pd.DataFrame(data=None))
+f1_f8_lgb_re10, f1_f8_pu_re10, f1_f1_lgb_re10,  f1_f7_lgb_re10= (
+    pd.DataFrame(data=None), pd.DataFrame(data=None), pd.DataFrame(data=None), pd.DataFrame(data=None))
+
+#num = 200
+num = 100
+
+for re10 in range(0, 10):
+
+    Nops_df_re = Nops_all_filled.sample(n=len(Pas_all_filled), random_state=re10)
+    
+    accuracy_f8_lgb, accuracy_f8_pu, accuracy_f1_lgb, accuracy_f7_lgb = [], [], [], []
+    f1_f8_lgb, f1_f8_pu, f1_f1_lgb, f1_f7_lgb = [], [], [], []
+
+    for i in range(0,10):
+
+        ###for lgb f8
+        test_df_pos = Pas_all_filled.sample(n=num, random_state=i)
+        train_df_pos = Pas_all_filled.drop(labels=test_df_pos.index)
+        test_df_neg = Nops_df_re.sample(n=num, random_state=i)
+        train_df_neg = Nops_df_re.drop(labels=test_df_neg.index)
+
+        training_df = pd.concat([train_df_pos, train_df_neg])
+        X_train = np.array(training_df.drop("Score", axis=1))
+        Y_train = np.array(training_df.Score)
+
+        x_test_pos = np.array(test_df_pos.drop("Score", axis=1))
+        y_test_pos = np.array(test_df_pos.Score)
+        x_test_neg = np.array(test_df_neg.drop("Score", axis=1))
+        y_test_neg = np.array(test_df_neg.Score)
+
+        x_test = np.concatenate((x_test_pos, x_test_neg))
+        y_test = np.concatenate((y_test_pos, y_test_neg))
+
+        _, _, f1_all_f8_lgb = GetProdictPS_lgb(X_train, Y_train, x_test, y_test)
+        accuracy_pos_f8_lgb, _, _ = GetProdictPS_lgb(X_train, Y_train, x_test_pos, y_test_pos)
+
+        accuracy_f8_lgb.append(accuracy_pos_f8_lgb)
+        f1_f8_lgb.append(f1_all_f8_lgb)
+
+        ###for pu f8
+        _, _, f1_all_f8_pu = GetProdictPS_pu(x_test, X_train, y_test, Y_train)
+        accuracy_pos_f8_pu, _, _ = GetProdictPS_pu(x_test_pos, X_train, y_test_pos, Y_train)
+
+        accuracy_f8_pu.append(accuracy_pos_f8_pu)
+        f1_f8_pu.append(f1_all_f8_pu)
+
+        ###for lgb f1
+        test_df_pos_f1 = test_df_pos.iloc[:, 7:9]
+        train_df_pos_f1 = train_df_pos.iloc[:, 7:9]
+        test_df_neg_f1 = test_df_neg.iloc[:, 7:9]
+        train_df_neg_f1 = train_df_neg.iloc[:, 7:9]
+
+        training_df_f1 = pd.concat([train_df_pos_f1, train_df_neg_f1])
+        X_train_f1 = np.array(training_df_f1.drop("Score", axis=1))
+        Y_train_f1 = np.array(training_df_f1.Score)
+
+        x_test_pos_f1 = np.array(test_df_pos_f1.drop("Score", axis=1))
+        y_test_pos_f1 = np.array(test_df_pos_f1.Score)
+        x_test_neg_f1 = np.array(test_df_neg_f1.drop("Score", axis=1))
+        y_test_neg_f1 = np.array(test_df_neg_f1.Score)
+
+        x_test_f1 = np.concatenate((x_test_pos_f1, x_test_neg_f1))
+        y_test_f1 = np.concatenate((y_test_pos_f1, y_test_neg_f1))
+
+        _, _, f1_all_f1_lgb = GetProdictPS_lgb(X_train_f1, Y_train_f1, x_test_f1, y_test_f1)
+        accuracy_pos_f1_lgb, _, _ = GetProdictPS_lgb(X_train_f1, Y_train_f1, x_test_pos_f1, y_test_pos_f1)
+
+        accuracy_f1_lgb.append(accuracy_pos_f1_lgb)
+        f1_f1_lgb.append(f1_all_f1_lgb)
+
+        ###for lgb f7
+        test_df_pos_f7 = test_df_pos.drop("PhosFreq", axis=1)
+        train_df_pos_f7 = train_df_pos.drop("PhosFreq", axis=1)
+        test_df_neg_f7 = test_df_neg.drop("PhosFreq", axis=1)
+        train_df_neg_f7 = train_df_neg.drop("PhosFreq", axis=1)
+
+        training_df_f7 = pd.concat([train_df_pos_f7, train_df_neg_f7])
+        X_train_f7 = np.array(training_df_f7.drop("Score", axis=1))
+        Y_train_f7 = np.array(training_df_f7.Score)
+
+        x_test_pos_f7 = np.array(test_df_pos_f7.drop("Score", axis=1))
+        y_test_pos_f7 = np.array(test_df_pos_f7.Score)
+        x_test_neg_f7 = np.array(test_df_neg_f7.drop("Score", axis=1))
+        y_test_neg_f7 = np.array(test_df_neg_f7.Score)
+
+        x_test_f7 = np.concatenate((x_test_pos_f7, x_test_neg_f7))
+        y_test_f7 = np.concatenate((y_test_pos_f7, y_test_neg_f7))
+
+        _, _, f1_all_f7_lgb = GetProdictPS_lgb(X_train_f7, Y_train_f7, x_test_f7, y_test_f7)
+        accuracy_pos_f7_lgb, _, _ = GetProdictPS_lgb(X_train_f7, Y_train_f7, x_test_pos_f7, y_test_pos_f7)
+
+        accuracy_f7_lgb.append(accuracy_pos_f7_lgb)
+        f1_f7_lgb.append(f1_all_f7_lgb)
+
+    accuracy_f8_lgb_re10[re10] = accuracy_f8_lgb
+    accuracy_f8_pu_re10[re10] = accuracy_f8_pu
+    accuracy_f1_lgb_re10[re10] = accuracy_f1_lgb
+    accuracy_f7_lgb_re10[re10] = accuracy_f7_lgb
+
+    f1_f8_lgb_re10[re10], f1_f8_pu_re10[re10], f1_f1_lgb_re10[re10], f1_f7_lgb_re10[re10] = (
+        f1_f8_lgb, f1_f8_pu, f1_f1_lgb, f1_f7_lgb)
+
+accuracy_mean_f8_lgb = np.array(accuracy_f8_lgb_re10.values.mean())
+accuracy_mean_f8_pu = np.array(accuracy_f8_pu_re10.values.mean())
+accuracy_mean_f1_lgb = np.array(accuracy_f1_lgb_re10.values.mean())
+accuracy_mean_f7_lgb = np.array(accuracy_f7_lgb_re10.values.mean())
+
+f1_score_mean_f8_lgb = np.array(f1_f8_lgb_re10.values.mean())
+f1_score_mean_f8_pu = np.array(f1_f8_pu_re10.values.mean())
+f1_score_mean_f1_lgb = np.array(f1_f1_lgb_re10.values.mean())
+f1_score_mean_f7_lgb = np.array(f1_f7_lgb_re10.values.mean())
+
+accuracy_mean = np.transpose(np.array([accuracy_mean_f1_lgb, accuracy_mean_f7_lgb, accuracy_mean_f8_lgb, accuracy_mean_f8_pu]))
+f1_score_mean = np.transpose(np.array([f1_score_mean_f1_lgb, f1_score_mean_f7_lgb, f1_score_mean_f8_lgb, f1_score_mean_f8_pu]))
+
+
+Auu = pd.DataFrame({
+                "TPR": accuracy_mean.flatten(),
+                "F1score": f1_score_mean.flatten()
+            }, columns=["TPR", "F1score"], index=["F1_lgb", "F7_lgb", "F8_lgb", "F8_pu"])
+            
+Auu.to_csv("PPF_predicted_Figure3c.csv")
---
+++ Figure3_PPF/Recall_7features+PPF_Figure3d.py
@@ -0,0 +1,140 @@
+import argparse, pickle, os, joblib
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle, resample
+from baggingPU import BaggingClassifierPU
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, recall_score, f1_score, mean_squared_error, confusion_matrix
+
+def random_undersampling(tmp_df, TARGET_LABEL):
+    df_majority = tmp_df[tmp_df[TARGET_LABEL] == 0]
+    df_minority = tmp_df[tmp_df[TARGET_LABEL] == 1]
+    df_majority_downsampled = resample(df_majority,
+                                       replace=False,  # sample without replacement
+                                       n_samples=len(df_minority),  # to match minority class
+                                       random_state=None)  # reproducible results
+    df_downsampled = pd.concat([df_majority_downsampled, df_minority])
+    print("Undersampling complete!")
+    print(df_downsampled[TARGET_LABEL].value_counts())
+    return df_downsampled
+
+param = {'objective':'binary',
+         'boosting':'dart'}
+
+Nops_df_org_real = pd.read_csv("NonPS_7features+PPF.csv", index_col=0)
+Ps_df_org_real = pd.read_csv("PS_7features+PPF.csv", index_col=0)
+
+Nops_df_org_pred = pd.read_csv("NonPS_7features+PPF_predicted.csv", index_col=0)
+Ps_df_org_pred = pd.read_csv("PS_7features+PPF_predicted.csv", index_col=0)
+
+Nops_df_org = pd.concat([Nops_df_org_real, Nops_df_org_pred])
+Ps_df_org = pd.concat([Ps_df_org_real, Ps_df_org_pred])
+
+mean_values_ps = Ps_df_org.mean()
+Pas_all_filled = Ps_df_org.fillna(mean_values_ps)
+mean_values_no = Nops_df_org.mean()
+Nops_all_filled = Nops_df_org.fillna(mean_values_no)
+
+sample_num, hidden_size = 400, 200
+accuracy_df, recall_df, f1_score_df = (
+    pd.DataFrame(data=None),
+    pd.DataFrame(data=None),
+    pd.DataFrame(data=None))
+accuracy_df_new, recall_df_new, f1_score_df_new = (
+    pd.DataFrame(data=None),
+    pd.DataFrame(data=None),
+    pd.DataFrame(data=None))
+
+for re10 in range(0, 10):
+    Nops_df_re = Nops_all_filled.sample(n=len(Pas_all_filled), random_state=re10)
+
+    test_df_pos_raw = Pas_all_filled.sample(n=sample_num, random_state=re10)
+    test_df_neg_raw = Nops_df_re.sample(n=sample_num, random_state=re10)
+    df_raw = pd.concat([test_df_pos_raw, test_df_neg_raw])
+    print(df_raw.Score.value_counts())
+
+    df_downsampled = random_undersampling(df_raw, 'Score')
+    df_downsampled = df_downsampled.sample(frac=1)
+    df_downsampled = df_downsampled.reset_index()
+    df_downsampled = df_downsampled.drop(columns=['UniproEntry'])
+    df = df_downsampled.copy()
+    NON_LBL = [c for c in df.columns if c != 'Score']
+    X = df[NON_LBL]
+    y = df['Score']
+
+    # Save the original labels and indices
+    y_orig = y.copy()
+    original_idx = np.where(df_downsampled.Score == 1)
+
+    # Here we are imputing 400 positives as negative
+    y.loc[np.random.choice(y[y == 1].index, replace=False, size=hidden_size)] = 0
+    y.value_counts()
+
+    accuracy, precision, recall, f1_score_ = [], [], [], []
+    accuracy_new, precision_new, recall_new, f1_score_new = [], [], [], []
+
+    ### lgb
+    classifier = LGBMClassifier(objective='binary', boosting_type='dart')
+    rf = classifier
+    rf.fit(X, y)
+
+    accuracy_lgb = accuracy_score(y_orig, rf.predict(X))
+    recall_lgb = recall_score(y_orig, rf.predict(X))
+    f1_score_lgb = f1_score(y_orig, rf.predict(X))
+
+    accuracy.append(accuracy_lgb)
+    recall.append(recall_lgb)
+    f1_score_.append(f1_score_lgb)
+
+    accuracy_df[re10] = accuracy
+    recall_df[re10] = recall
+    f1_score_df[re10] = f1_score_
+
+    ### pu
+    cf = BaggingClassifierPU(
+        classifier,
+        n_estimators=100,
+        max_samples=100,
+        bootstrap=True,
+        oob_score=True,
+        n_jobs=-1
+    )
+    cf.fit(X, y)
+
+    accuracy_pu = accuracy_score(y_orig, cf.predict(X))
+    recall_pu = recall_score(y_orig, cf.predict(X))
+    f1_score_pu = f1_score(y_orig, cf.predict(X))
+
+    accuracy_new.append(accuracy_pu)
+    recall_new.append(recall_pu)
+    f1_score_new.append(f1_score_pu)
+
+    accuracy_df_new[re10] = accuracy_new
+    recall_df_new[re10] = recall_new
+    f1_score_df_new[re10] = f1_score_new
+
+
+########################################
+
+accuracy_mean_mod = np.array(accuracy_df.mean(axis=1))
+accuracy_mean_pu = np.array(accuracy_df_new.mean(axis=1))
+recall_mean_mod = np.array(recall_df.mean(axis=1))
+recall_mean_pu = np.array(recall_df_new.mean(axis=1))
+f1_score_mean_mod = np.array(f1_score_df.mean(axis=1))
+f1_score_mean_pu = np.array(f1_score_df_new.mean(axis=1))
+
+lgb_mean = np.transpose(np.array([recall_mean_mod, f1_score_mean_mod, accuracy_mean_mod]))
+pu_mean = np.transpose(np.array([recall_mean_pu, f1_score_mean_pu, accuracy_mean_pu]))
+
+Auu = pd.DataFrame({
+                "lgb": lgb_mean.flatten(),
+                "pu": pu_mean.flatten()
+            }, columns=["lgb", "pu"], index=["Recall", "F1_score", "Accuracy"])
+Auu.to_csv("Recall_7features+PPF_Figure3d.csv")
+
