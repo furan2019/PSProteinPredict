01232a1f048cfdb3840f001a331e9146ae0d4265
furan2019 <51429163+furan2019@users.noreply.github.com>
2025-11-04 23:33:37 +0800

    Update whole_pro_LGB_score_step1-2_code1.py
    data updating

--- Figure4_wholePro/whole_pro_LGB_score_step1-2_code1.py
+++ Figure4_wholePro/whole_pro_LGB_score_step1-2_code1.py
@@ -38,11 +38,26 @@ mean_values_no = Nops_all.mean()
 Nops_all_filled = Nops_all.fillna(mean_values_no)
 
 #
+verified_positive = pd.read_csv("CorrectedData_positive_755.csv", index_col=0)
+mean_values_verified_positive = verified_positive.mean()
+verified_positive_filled = verified_positive.fillna(mean_values_verified_positive)
+
+verified_samples = verified_positive_filled.sample(n=700, random_state=0)
+verified_remaining = verified_positive_filled.drop(verified_samples.index)
+verified_remaining.to_csv("verified_test.csv")
+verified_samples.to_csv("verified_samples.csv")
+
 positive_samples_ = Pas_all_filled.sample(n=6000, random_state=0) #
 positive_index = positive_samples_.index
 positive_test = Pas_all_filled.drop(labels=positive_index)
 positive_test.to_csv("positive_test.csv")
 positive_samples_.to_csv("positive_samples_.csv")
+
+#oversampling
+verified_oversampled = resample(verified_samples, 
+                               replace=True, 
+                               n_samples=len(positive_samples_), 
+                               random_state=0)
 
 background_samples_ = Nops_all_filled.sample(n=144000, random_state=1)  #
 background_index = background_samples_.index
@@ -55,20 +70,22 @@ protein_predictions = {}
 for i in background_index:
     protein_predictions[i] = []
 
-positive_samples = np.array(positive_samples_.drop("Score", axis=1))
+#
+positive_samples_combined = pd.concat([positive_samples_, verified_oversampled])
+positive_samples = np.array(positive_samples_combined.drop("Score", axis=1))
 background_samples = np.array(background_samples_.drop("Score", axis=1))
 
 #
-for i in range(0, len(background_samples), len(positive_samples)):
-    neg_train_samples = background_samples[i:i+len(positive_samples)]
+for i in range(0, len(background_samples), len(positive_samples_combined)):
+    neg_train_samples = background_samples[i:i+len(positive_samples_combined)]
     train_samples = np.concatenate((positive_samples, neg_train_samples), axis=0)
 
     lgb_train = lgb.Dataset(train_samples, label=np.concatenate((np.ones(len(positive_samples)),
                                                                  np.zeros(len(neg_train_samples)))))
     model = lgb.train(params, lgb_train)
     
-    rest_neg_index = np.delete(background_index, np.arange(i, i + len(positive_samples)))
-    rest_neg_samples = np.delete(background_samples, np.arange(i, i + len(positive_samples)), axis=0)
+    rest_neg_index = np.delete(background_index, np.arange(i, i + len(positive_samples_combined)))
+    rest_neg_samples = np.delete(background_samples, np.arange(i, i + len(positive_samples_combined)), axis=0)
 
     rest_predictions = model.predict(rest_neg_samples)
     
@@ -85,7 +102,7 @@ for protein_index, predictions in protein_predictions.items():
     average_protein_predictions[protein_index] = average_prediction
 
 #
-output_file = 'LGB_prediction_score.csv'
+output_file = 'LGB_prediction_score_CorrectionData.csv'
 with open(output_file, 'w') as f:
     f.write("ProteinIndex,AveragePrediction\n")
     for protein_index, average_prediction in average_protein_predictions.items():
