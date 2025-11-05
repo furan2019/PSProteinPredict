d1487364963c21377228ac7565bc9eb75c276923
furan2019 <51429163+furan2019@users.noreply.github.com>
2025-11-05 10:14:56 +0800

    Update and rename whole_pro_PU_reliabilityclass_step3_2_code4.py to whole_pro_PU_reliabilityclass_step4_code4.py
    data updating

--- Figure4_wholePro/whole_pro_PU_reliabilityclass_step4_code4.py
+++ Figure4_wholePro/whole_pro_PU_reliabilityclass_step4_code4.py
@@ -1,4 +1,4 @@
-### STEP3_2 ### AFTER STEP4
+### STEP4 ###
 
 import numpy as np
 from lightgbm import LGBMClassifier
@@ -8,15 +8,32 @@ from baggingPU import BaggingClassifierPU
 from sklearn.externals import joblib
 from sklearn.metrics import accuracy_score, f1_score
 
+#positive training data
 positive_samples_ = pd.read_csv("positive_samples_.csv", index_col=0)
+verified_samples = pd.read_csv("verified_samples.csv", index_col=0)
+positive_samples_700 = positive_samples_.sample(n=len(verified_samples), random_state=2025)
+#
+positive_samples_combined = pd.concat([positive_samples_700, verified_samples])
 
+#positive validation data
 positive_valid = pd.read_csv("positive_test.csv", index_col=0)
-background_valid = pd.read_csv("background_test.csv", index_col=0)
-background_valid_ = background_valid.sample(n=len(positive_valid), random_state=2024)
-data_valid = pd.concat([positive_valid, background_valid_])
+verified_remaining = pd.read_csv("verified_test.csv", index_col=0)
+positive_valid_55 = positive_valid.sample(n=len(verified_remaining), random_state=2025)
+#
+positive_valid_all = pd.concat([positive_valid_55, verified_remaining])
 
-average_protein_predictions = pd.read_csv("OOB_score.csv", index_col=0)
-background_samples_ = pd.read_csv("background_samples_.csv", index_col=0) #特征文件
+#negative data
+negative_valid_ = pd.read_csv("CorrectedData_negative_2121.csv", index_col=0)
+mean_negative_valid = negative_valid_.mean()
+negative_valid = negative_valid_.fillna(mean_negative_valid)
+
+negative_valid_all = negative_valid.sample(n=len(positive_valid_all), random_state=2024) 
+negative_train = negative_valid.sample(n=len(positive_samples_combined), random_state=2025)
+
+data_valid = pd.concat([positive_valid_all, negative_valid_all])
+
+average_protein_predictions = pd.read_csv("PU_prediction_score.csv", index_col=0)
+background_samples_ = pd.read_csv("background_samples_.csv", index_col=0)
 background_index = background_samples_.index
 
 condition_ = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
@@ -37,24 +54,33 @@ for mmm in range(9):
         ps_final_data['Score'] = 1
 
         negative_samples = X_train_neg_final
-        positive_samples = pd.concat([positive_samples_, ps_final_data])
+        positive_samples = ps_final_data
 
-        train_num = 6000
+        #
+        train_predict_num = 700
         for i in range(10):
-            # sampled_neg_data = negative_samples.sample(n=train_num, random_state=i)
-            # sampled_pos_data = positive_samples.sample(n=train_num, random_state=i)
-            sampled_neg_data = negative_samples.sample(n=train_num)
-            sampled_pos_data = positive_samples.sample(n=train_num)
+            if train_predict_num <= len(positive_samples):
+                sampled_pos_data = positive_samples.sample(n=train_predict_num, replace=False)
 
-            train_data = pd.concat([sampled_pos_data, sampled_neg_data])
+            else:
+                sampled_pos_data = positive_samples.sample(n=train_predict_num, replace=True)
+                
+            if train_predict_num <= len(positive_samples):
+                sampled_neg_data = negative_samples.sample(n=train_predict_num, replace=False)
 
-            #base_estimator = LGBMClassifier(objective='binary', boosting_type='dart', random_state = 2)
+            else:
+                sampled_neg_data = negative_samples.sample(n=train_predict_num, replace=True)
+
+            train_positive = pd.concat([positive_samples_combined, sampled_pos_data]) #1400+700
+            train_negative = pd.concat([negative_train, sampled_neg_data]) #1400+700
+
+            train_data = pd.concat([train_positive, train_negative])
+
             base_estimator = LGBMClassifier(objective='binary', boosting_type='dart', learning_rate=0.5,
                                             bagging_fraction=0.5,
                                             feature_fraction=0.8, min_child_samples=18, num_leaves=5, random_state=2)
-            model = BaggingClassifierPU(base_estimator, random_state=2)
             
-            #model = BaggingClassifierPU(base_estimator, n_estimators=100, max_samples=100, bootstrap=True, oob_score=True, n_jobs=-1)
+            model = BaggingClassifierPU(base_estimator, random_state=2)
             model.fit(train_data.drop('Score', axis=1), train_data['Score'])
 
             joblib.dump(model, f'pu_bagging_model222_{i + 1}.joblib')
